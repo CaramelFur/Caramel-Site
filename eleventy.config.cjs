f360e61c9834d7ce31515e8cbfaea6967b3b5c13
rubikscraft <contact@rubikscraft.nl>
2023-01-15 13:46:59 +0100

    remove unnessary log

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -2,4 +2,3 @@ const deasyncPromise = require('deasync-promise');
 const importPromise = import('./lib/build.js');
 
 module.exports = deasyncPromise(importPromise).default;
-console.log(module.exports);
